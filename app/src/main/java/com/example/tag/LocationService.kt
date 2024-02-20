97ab9506b2681b14cef8a46bb2f99e15fd8c9a05
mdtaha <taha.papa089@gmail.com>
2024-02-21 02:43:01 +0530

    First Commit

--- app/src/main/java/com/example/tag/LocationService.kt
+++ app/src/main/java/com/example/tag/LocationService.kt
@@ -48,69 +48,70 @@ class LocationService: Service() {
 
     private fun start() {
 
-//
-//        val notification = NotificationCompat.Builder(this, "location11")
-//            .setContentTitle("Tracking location...")
-//            .setContentText("Location: null")
-//            .setSmallIcon(R.drawable.ic_launcher_background)
-//            .setOngoing(true)
-//
-//        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//
-//        locationClient
-//            .getLocationUpdates(10000L)
-//            .catch { e -> e.printStackTrace() }
-//            .onEach { location ->
-//                val lat = location.latitude.toString().takeLast(3)
-//                val long = location.longitude.toString().takeLast(3)
-//                val updatedNotification = notification.setContentText(
-//                    "Location: ($lat, $long)"
-//                )
-//                notificationManager.notify(1, updatedNotification.build())
-//            }
-//            .launchIn(serviceScope)
-//
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-//            startForeground(1, notification.build())
-//        } else {
-//            startForeground(1, notification.build())
-//
-//        }
-//        startForeground(1, notification.build())
 
-        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification = NotificationCompat.Builder(this, "location11")
+            .setContentTitle("Tracking location...")
+            .setContentText("Location: null")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setOngoing(true)
 
-        val chan = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel(
-                "NOTIFICATION_CHANNEL_ID",
-                "channelName",
-                NotificationManager.IMPORTANCE_NONE
-            )
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        locationClient
+            .getLocationUpdates(10000L)
+            .catch { e -> e.printStackTrace() }
+            .onEach { location ->
+                val lat = location.latitude.toString().takeLast(3)
+                val long = location.longitude.toString().takeLast(3)
+                val updatedNotification = notification.setContentText(
+                    "Location: ($lat, $long)"
+                )
+                notificationManager.notify(1, updatedNotification.build())
+            }
+            .launchIn(serviceScope)
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            startForeground(1, notification.build())
         } else {
-            TODO("VERSION.SDK_INT < O")
+            startForeground(1, notification.build())
+
         }
-        chan.description = "for muting"
+        startForeground(1, notification.build())
 
-        manager.createNotificationChannel(chan)
+//        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//
+//        val chan = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationChannel(
+//                "NOTIFICATION_CHANNEL_ID",
+//                "channelName",
+//                NotificationManager.IMPORTANCE_NONE
+//            )
+//        } else {
+//            TODO("VERSION.SDK_INT < O")
+//        }
+//        chan.description = "for muting"
+//
+//        manager.createNotificationChannel(chan)
+//
+//        manager.createNotificationChannel(
+//            NotificationChannel(
+//                "name",
+//                "Muter",
+//                NotificationManager.IMPORTANCE_DEFAULT
+//            )
+//        )
+//
+////        Log.d("TAG", "success" )
+//
+//        val notificationBuilder = NotificationCompat.Builder(this, "NOTIFICATION_CHANNEL_ID")
+//        val notification = notificationBuilder.setOngoing(true)
+//            .setContentTitle("appname")
+//            .setPriority(NotificationManager.IMPORTANCE_MIN)
+//            .setCategory(Notification.CATEGORY_SERVICE)
+//            .build()
+//
+//        startForeground(4, notification)
 
-        manager.createNotificationChannel(
-            NotificationChannel(
-                "name",
-                "Muter",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-        )
-
-//        Log.d("TAG", "success" )
-
-        val notificationBuilder = NotificationCompat.Builder(this, "NOTIFICATION_CHANNEL_ID")
-        val notification = notificationBuilder.setOngoing(true)
-            .setContentTitle("appname")
-            .setPriority(NotificationManager.IMPORTANCE_MIN)
-            .setCategory(Notification.CATEGORY_SERVICE)
-            .build()
-
-        startForeground(4, notification)
     }
 
 
